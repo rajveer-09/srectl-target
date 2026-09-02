7feb38a6a74f0e6741fdf10c2e3f89887954cbf7
rajveer-09 <rajveersharma1107@gmail.com>
2026-09-02 19:30:12 +0530

    Add percentage discount helpers

---
+++ src/discount.ts
@@ -0,0 +1,10 @@
+﻿import { toCents } from "./money.js";
+
+/** Percentage discount applied to an integer-cent amount. */
+export function applyDiscount(cents: number, rate: number): number {
+  return Math.round(cents * (1 - rate));
+}
+
+export function discountFromPrice(price: number, rate: number): number {
+  return applyDiscount(toCents(price), rate);
+}
